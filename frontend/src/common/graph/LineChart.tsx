bedd1b80ab22abb17e771ef78e08302d6d695050
Harjot Singh <hsingh172000@gmail.com>
2024-01-05 23:03:03 -0800

    Cleaned up code. Got better version of tooltip working

--- frontend/src/common/graph/LineChart.tsx
+++ frontend/src/common/graph/LineChart.tsx
@@ -1,4 +1,6 @@
 import {
+  ScaleLinear,
+  ScaleTime,
   extent,
   line,
   scaleLinear,
@@ -10,15 +12,9 @@ import React, { FC, useState } from "react";
 import useDetectMobile from "../../hooks/useDetectMobile";
 import { Constants } from "../../utils/constants";
 import { Nullable, SpendingListRow } from "../../utils/types";
-import GraphTooltip from "./GraphTooltip";
-import DateUtils from "../../utils/date-utils";
-import MoneyUtils from "../../utils/money-utils";
 
-type LineChartProps = {
-  width: number;
-  height: number;
-  data: SpendingListRow[];
-};
+const TRACER_X_INITIAL = -10;
+const POINT_RADIUS = 7;
 
 function calculateMargins(height: number, width: number) {
   return {
@@ -29,8 +25,132 @@ function calculateMargins(height: number, width: number) {
   };
 }
 
-const TRACER_X_INITIAL = -10;
-const POINT_RADIUS = 7;
+type YTicksProps = {
+  yTicks: number[];
+  x1: number;
+  x2: number;
+  yScale: ScaleLinear<number, number, never>;
+};
+
+const YTicks: FC<YTicksProps> = ({ x1, x2, yScale, yTicks }) => {
+  return (
+    <>
+      {yTicks.map((value) => {
+        return (
+          <line
+            key={value}
+            x1={x1}
+            x2={x2}
+            y1={yScale(value)}
+            y2={yScale(value)}
+            stroke="gray"
+            strokeDasharray={"1,7"}
+            strokeLinecap="round"
+          />
+        );
+      })}
+    </>
+  );
+};
+
+type TracerProps = {
+  height: number;
+  x: number;
+};
+
+const Tracer: FC<TracerProps> = ({ height, x }) => {
+  return <rect width={2} height={height} x={x} fill="gray" />;
+};
+
+type LineProps = {
+  d: Nullable<string>;
+};
+
+const Line: FC<LineProps> = ({ d }) => {
+  if (!d) {
+    return <></>;
+  }
+
+  return (
+    <path
+      className="animate-[linechart_1.5s_cubic-bezier(1,0,0,1)_forwards]"
+      d={d}
+      fill="none"
+      stroke="#00ADB5"
+      strokeWidth={2}
+      strokeDasharray={"10000"}
+      strokeDashoffset={-10000}
+    />
+  );
+};
+
+type PointProps = {
+  spendingListRow: SpendingListRow;
+  setTooltipDate: (date: Nullable<string>) => void;
+  xScale: ScaleTime<number, number, never>;
+  yScale: ScaleLinear<number, number, never>;
+};
+
+const Point: FC<PointProps> = ({
+  setTooltipDate,
+  spendingListRow,
+  xScale,
+  yScale,
+}) => {
+  const parser = timeParse(Constants.ISO_FORMAT);
+  return (
+    <circle
+      key={spendingListRow.date}
+      className="hover:cursor-pointer"
+      onMouseOver={() => setTooltipDate(spendingListRow.date)}
+      onMouseLeave={() => setTooltipDate(null)}
+      fill="white"
+      stroke="#374151"
+      strokeWidth={5}
+      cx={xScale(parser(spendingListRow.date)!)}
+      cy={yScale(spendingListRow.total)}
+      r={POINT_RADIUS}
+    />
+  );
+};
+
+type XTicksProps = {
+  xTicks: Date[];
+  xScale: ScaleTime<number, number, never>;
+  y: number;
+};
+
+const XTicks: FC<XTicksProps> = ({ xTicks, xScale, y }) => {
+  const xAxisFormatter = timeFormat("%m/%Y");
+  return (
+    <>
+      {xTicks.map((date) => {
+        return (
+          <text
+            key={date.toDateString()}
+            className="font-semibold"
+            fill="#EEEEEE"
+            x={xScale(date)}
+            y={y}
+          >
+            {xAxisFormatter(date)}
+          </text>
+        );
+      })}
+    </>
+  );
+};
+
+type TooltipPosition = {
+  top: number;
+  left: number;
+};
+
+type LineChartProps = {
+  width: number;
+  height: number;
+  data: SpendingListRow[];
+};
 
 const LineChart: FC<LineChartProps> = ({ data, height, width }) => {
   const parser = timeParse(Constants.ISO_FORMAT);
@@ -38,6 +158,8 @@ const LineChart: FC<LineChartProps> = ({ data, height, width }) => {
   const isMobile = useDetectMobile();
   const [tracerX, setTracerX] = useState<number>(TRACER_X_INITIAL);
   const [tooltipDate, setTooltipDate] = useState<Nullable<string>>(null);
+  const [tooltipPosition, setTooltipPosition] =
+    useState<Nullable<TooltipPosition>>(null);
 
   const moveTracer = (e: React.MouseEvent) => {
     const domPoint = new DOMPointReadOnly(e.clientX, e.clientY);
@@ -46,17 +168,25 @@ const LineChart: FC<LineChartProps> = ({ data, height, width }) => {
       svgNode.getScreenCTM()?.inverse()
     );
     setTracerX(svgPoint.x);
-    
+
     // If tracer hovers over a circle then show tooltip
     let date: Nullable<string> = null;
-    data.forEach((spendingListRow) => {
+    let pos: Nullable<TooltipPosition> = null;
+
+    for (let spendingListRow of data) {
       const d = Math.floor(xScale(parser(spendingListRow.date)!) - svgPoint.x);
-      if (Math.abs(d) <= POINT_RADIUS) {
+      if (Math.abs(d) <= POINT_RADIUS * 2) {
         date = spendingListRow.date;
-      } 
-    });
+        pos = {
+          left: svgPoint.x,
+          top: yScale(spendingListRow.total),
+        };
+        break;
+      }
+    }
 
     setTooltipDate(date);
+    setTooltipPosition(pos);
   };
 
   const xScale = scaleTime()
@@ -75,8 +205,6 @@ const LineChart: FC<LineChartProps> = ({ data, height, width }) => {
 
   const d = lineFn(data);
 
-  const xAxisFormatter = timeFormat("%m/%Y");
-
   const xTicks = isMobile
     ? xScale.ticks(Math.floor(xScale.ticks().length / 3))
     : xScale.ticks(Math.floor(xScale.ticks().length / 2));
@@ -84,95 +212,53 @@ const LineChart: FC<LineChartProps> = ({ data, height, width }) => {
   const yTicks = yScale.ticks(yScale.ticks().length / 2);
 
   return (
-    <svg
-      height={height}
-      width={width}
-      onMouseMove={(e: React.MouseEvent) => moveTracer(e)}
-      onMouseLeave={() => setTracerX(TRACER_X_INITIAL)}
-    >
-      <g>
-        {yTicks.map((value) => {
-          return (
-            <line
-              key={value}
-              x1={margins.left}
-              x2={width - margins.right}
-              y1={yScale(value)}
-              y2={yScale(value)}
-              stroke="gray"
-              strokeDasharray={"1,7"}
-              strokeLinecap="round"
+    <div className="relative">
+      <svg
+        height={height}
+        width={width}
+        onMouseMove={(e: React.MouseEvent) => moveTracer(e)}
+        onMouseLeave={() => setTracerX(TRACER_X_INITIAL)}
+      >
+        <g>
+          <YTicks
+            x1={margins.left}
+            x2={width - margins.right}
+            yScale={yScale}
+            yTicks={yTicks}
+          />
+
+          <Tracer height={height - margins.top} x={tracerX} />
+          <Line d={d} />
+
+          {data.map((spendingListRow) => (
+            <Point
+              key={spendingListRow.date}
+              spendingListRow={spendingListRow}
+              setTooltipDate={setTooltipDate}
+              xScale={xScale}
+              yScale={yScale}
             />
-          );
-        })}
+          ))}
 
-        <rect width={2} height={height - margins.top} x={tracerX} fill="gray" />
-        <path
-          className="animate-[linechart_1.5s_cubic-bezier(1,0,0,1)_forwards]"
-          d={d ? d : ""}
-          fill="none"
-          stroke="#00ADB5"
-          strokeWidth={2}
-          strokeDasharray={"10000"}
-          strokeDashoffset={-10000}
-        />
+          <XTicks xScale={xScale} xTicks={xTicks} y={height - margins.bottom} />
+        </g>
+      </svg>
 
-        {data.map((spendingListRow) => {
-          return (
-            <g className="relative" key={spendingListRow.date}>
-              <circle
-                key={spendingListRow.date}
-                className="hover:cursor-pointer"
-                onMouseOver={() => setTooltipDate(spendingListRow.date)}
-                onMouseLeave={() => setTooltipDate(null)}
-                fill="white"
-                stroke="#374151"
-                strokeWidth={5}
-                cx={xScale(parser(spendingListRow.date)!)}
-                cy={yScale(spendingListRow.total)}
-                r={POINT_RADIUS}
-              />
-
-              {tooltipDate === spendingListRow.date && (
-                <GraphTooltip
-                  w={100}
-                  h={100}
-                  x={xScale(parser(spendingListRow.date)!)}
-                  y={yScale(spendingListRow.total) - 50}
-                >
-                  <text
-                    x={xScale(parser(spendingListRow.date)!)}
-                    y={yScale(spendingListRow.total)}
-                    className="font-semibold text-white"
-                  >
-                    <tspan x={xScale(parser(spendingListRow.date)!)}>
-                      Date: {DateUtils.formatDateUS(spendingListRow.date)}
-                    </tspan>
-                    <tspan x={xScale(parser(spendingListRow.date)!)} dy={20}>
-                      Amount: {MoneyUtils.formatMoneyUsd(spendingListRow.total)}
-                    </tspan>
-                  </text>
-                </GraphTooltip>
-              )}
-            </g>
-          );
-        })}
-
-        {xTicks.map((date) => {
-          return (
-            <text
-              key={date.toDateString()}
-              className="font-semibold"
-              fill="#EEEEEE"
-              x={xScale(date)}
-              y={height - margins.bottom}
-            >
-              {xAxisFormatter(date)}
-            </text>
-          );
-        })}
-      </g>
-    </svg>
+      <div
+        className={"w-fit h-fit bg-red-600 absolute"}
+        style={{
+          display: tooltipPosition ? "block" : "none",
+          top: tooltipPosition?.top,
+          left: tooltipPosition?.left,
+        }}
+      >
+        TEST
+        TEST
+        TEST
+        TEST
+        TEST
+      </div>
+    </div>
   );
 };
 
