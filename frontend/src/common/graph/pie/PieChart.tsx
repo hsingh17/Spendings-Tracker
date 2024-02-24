bbc52574267f0d64d914d808a8890cee3e9a3064
Harjot Singh <hsingh172000@gmail.com>
2024-02-23 22:28:37 -0800

    Adjust inner and outer radius based on window height

--- frontend/src/common/graph/pie/PieChart.tsx
+++ frontend/src/common/graph/pie/PieChart.tsx
@@ -1,5 +1,6 @@
-import { PieArcDatum, arc, interpolateCool, pie, scaleSequential } from "d3";
+import { PieArcDatum, arc, interpolateRgb, pie, scaleSequential } from "d3";
 import { FC } from "react";
+import useDetectMobile from "../../../hooks/useDetectMobile";
 import { ApiResponse, CategoricalSpendings } from "../../../utils/types";
 
 type PieChartProps = {
@@ -15,10 +16,11 @@ const PieChart: FC<PieChartProps> = ({ width, height, response }) => {
     return <>TODO</>;
   }
 
-  const innerRadius = 0;
-  const outerRadius = width / 10;
+  const isMobile = useDetectMobile();
+  const innerRadius = height / 4 + (isMobile ? 10 : 25);
+  const outerRadius = height / 2 - (isMobile ? 10 : 100);
   const interpolatorScale = scaleSequential()
-    .interpolator(interpolateCool)
+    .interpolator(interpolateRgb("#EEEEEE", "#00ADB5"))
     .domain([0, data.length]);
 
   const pieGenerator = pie<CategoricalSpendings>()
@@ -33,7 +35,15 @@ const PieChart: FC<PieChartProps> = ({ width, height, response }) => {
     <svg height={height} width={width}>
       <g style={{ transform: `translate(${width / 2}px, ${height / 2}px)` }}>
         {pieGenerator(data).map((d, i) => {
-          return <path d={arcGenerator(d) || ""} fill={interpolatorScale(i)} />;
+          return (
+            <path
+              className="hover:cursor-pointer"
+              d={arcGenerator(d) || ""}
+              fill={interpolatorScale(i)}
+              stroke="#374151"
+              strokeWidth={3}
+            />
+          );
         })}
       </g>
     </svg>
