14d8e808dc26841736a2ed01b66f6ffbbca38331
Harjot Singh <hsingh172000@gmail.com>
2024-08-14 21:30:13 -0700

    Finish JDBC repository

--- backend/src/main/java/com/spendingstracker/app/repository/SpendingUserJdbcRepository.java
+++ backend/src/main/java/com/spendingstracker/app/repository/SpendingUserJdbcRepository.java
@@ -10,13 +10,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class SpendingUserJdbcRepository {
@@ -39,44 +39,51 @@ public class SpendingUserJdbcRepository {
             LocalDate endDate,
             Granularity granularity,
             Pageable pageable) {
+        SqlParameterSource params = buildParams(userId, startDate, endDate, granularity, pageable);
+
         return queryForSpendingListProjs(
+                queryForSpendingsListProjsCount(
+                        sqlResourcesMap.get("countSpendingsNumericalGroupBy"), params),
                 sqlResourcesMap.get("findSpendingsNumericalGroupBy"),
-                userId,
-                startDate,
-                endDate,
-                granularity,
-                pageable);
+                pageable,
+                params);
     }
 
     public Page<SpendingListProjection> findSpendingsCategorical(
             BigInteger userId, LocalDate startDate, LocalDate endDate, Pageable pageable) {
+        SqlParameterSource params = buildParams(userId, startDate, endDate, null, pageable);
         return queryForSpendingListProjs(
+                queryForSpendingsListProjsCount(
+                        sqlResourcesMap.get("countSpendingsCategorical"), params),
                 sqlResourcesMap.get("findSpendingsCategorical"),
-                userId,
-                startDate,
-                endDate,
-                null,
-                pageable);
+                pageable,
+                params);
     }
 
     private Page<SpendingListProjection> queryForSpendingListProjs(
-            String sql,
+            int total, String sql, Pageable pageable, SqlParameterSource params) {
+
+        List<SpendingListProjection> projsList = jdbcTemplate.query(sql, params, rowMapper);
+        return new PageImpl<>(projsList, pageable, total); // TODO
+    }
+
+    private SqlParameterSource buildParams(
             BigInteger userId,
             LocalDate startDate,
             LocalDate endDate,
             Granularity granularity,
             Pageable pageable) {
-        SqlParameterSource params =
-                new MapSqlParameterSource()
-                        .addValue("userId", userId)
-                        .addValue("startDate", startDate)
-                        .addValue("endDate", endDate)
-                        .addValue("granularity", granularity)
-                        .addValue("limit", pageable.getPageSize())
-                        .addValue(
-                                "offset", (pageable.getPageNumber() - 1) * pageable.getPageSize());
+        return new MapSqlParameterSource()
+                .addValue("userId", userId)
+                .addValue("startDate", startDate)
+                .addValue("endDate", endDate)
+                .addValue("granularity", granularity)
+                .addValue("limit", pageable.getPageSize())
+                .addValue("offset", (pageable.getPageNumber() - 1) * pageable.getPageSize());
+    }
 
-        List<SpendingListProjection> projsList = jdbcTemplate.query(sql, params, rowMapper);
-        return new PageImpl<>(projsList, pageable, null); // TODO
+    private int queryForSpendingsListProjsCount(String sql, SqlParameterSource params) {
+        return Optional.ofNullable(jdbcTemplate.queryForObject(sql, params, Integer.class))
+                .orElse(0);
     }
 }
---
+++ backend/src/main/resources/sql/countSpendingsNumericalGroupBy.sql
@@ -0,0 +1,57 @@
+SELECT
+    COUNT(*)
+FROM
+    APP.SPENDING_USER_AGGR SUA,
+    APP.SPENDING S
+WHERE
+        SUA.SPENDING_USER_AGGR_ID = S.SPENDING_USER_AGGR_ID
+    AND SUA.USER_ID = :userId
+    AND SUA.DATE >= :startDate
+    AND SUA.DATE <= :endDate
+GROUP BY
+    (
+        CASE
+            WHEN :granulatity = "DAY" THEN (
+                DATE_FORMAT (SUA.DATE, "%Y-%m-%d") AS date
+            )
+            WHEN :granulatity = "WEEK" THEN (
+                DATE_FORMAT (
+                    DATE_SUB(SUA.DATE, INTERVAL WEEKDAY (SUA.DATE) DAY),
+                    "%Y-%m-%d"
+                )
+            )
+            WHEN :granulatity = "MONTH" THEN (
+                DATE_FORMAT(SUA.DATE, "%Y-%m-01") AS date,
+            )
+            WHEN :granulatity = "YEAR" THEN (
+                DATE_FORMAT(SUA.DATE, "%Y-01-01")
+            )
+            ELSE SUA.DATE
+        END
+    )
+ORDER BY
+    (
+        CASE
+            WHEN :granulatity = "DAY" THEN (
+                DATE_FORMAT (SUA.DATE, "%Y-%m-%d") AS date
+            )
+            WHEN :granulatity = "WEEK" THEN (
+                DATE_FORMAT (
+                    DATE_SUB(SUA.DATE, INTERVAL WEEKDAY (SUA.DATE) DAY),
+                    "%Y-%m-%d"
+                )
+            )
+            WHEN :granulatity = "MONTH" THEN (
+                DATE_FORMAT(SUA.DATE, "%Y-%m-01") AS date,
+            )
+            WHEN :granulatity = "YEAR" THEN (
+                DATE_FORMAT(SUA.DATE, "%Y-01-01")
+            )
+            ELSE SUA.DATE
+        END
+    )
+LIMIT
+    :limit
+OFFSET
+    :offset
+;
