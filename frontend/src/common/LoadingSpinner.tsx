60414180fdf5dd3ceea988fa81285263ecc6a26d
Harjot Singh <hsingh172000@gmail.com>
2024-09-13 22:08:01 -0700

    More work on spinner

--- frontend/src/common/LoadingSpinner.tsx
+++ frontend/src/common/LoadingSpinner.tsx
@@ -1,19 +1,32 @@
 const LoadingSpinner = () => {
+  const height = 1000;
+  const width = 1000;
+  const outerRadius = 100;
+  const innerRadius = 50;
   return (
-    <svg width={1000} height={1000}>
+    <svg width={width} height={height}>
       <g
         style={{
-          transform: `translate(500px, 500px)`,
+          scale: "1",
+          transform: `translate(${width / 2}px, ${height / 2}px)`,
           transformOrigin: "center center",
         }}
       >
+        <circle className="brightness-200" r={outerRadius} fill="#EEEEEE" />
         <circle
-          r={25}
-          fill="none"
-          stroke="red"
-          strokeWidth={5}
-          strokeDasharray={10000}
-          strokeDashoffset={10000} // this number start 10000 -> 0 for spinner animation
+          className="animate-spin"
+          r={innerRadius}
+          fill="#EEEEEE"
+          strokeWidth={1.5 * (outerRadius - innerRadius)}
+          stroke="#00ADB5"
+          strokeDasharray={9990}
+          strokeDashoffset={9920}
+        />
+        <circle
+          r={innerRadius}
+          fill="#EEEEEE"
+          stroke="#EEEEEE"
+          strokeWidth={outerRadius - innerRadius}
         />
       </g>
     </svg>
