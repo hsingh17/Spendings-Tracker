4eac3aa2727f15880869262c752770632e7cd237
Harjot Singh <hsingh172000@gmail.com>
2025-05-01 16:10:43 -0700

    Continue on more unit tests

--- backend/src/test/java/com/spendingstracker/app/controller/SpendingsControllerTest.java
+++ backend/src/test/java/com/spendingstracker/app/controller/SpendingsControllerTest.java
@@ -2,6 +2,7 @@ package com.spendingstracker.app.controller;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.spendingstracker.app.controller.spending.SpendingsController;
@@ -10,6 +11,7 @@ import com.spendingstracker.app.dto.response.SpendingPageResponse;
 import com.spendingstracker.app.service.spending.SpendingCategoryService;
 import com.spendingstracker.app.service.spending.SpendingService;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -35,44 +37,77 @@ public class SpendingsControllerTest {
         when(spendingService.getSpendings(any(GetSpendingsRequestFilters.class)))
                 .thenReturn(response);
 
-        mockMvc.perform(get("/v1/api/spendings")).andExpect(status().isOk());
+        mockMvc.perform(get("/v1/api/spendings"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(200)));
+        ;
     }
 
     @Test
     public void shouldThrowBadRequestForInvalidStartDate() throws Exception {
         mockMvc.perform(get("/v1/api/spendings?start-date=01/01/2000"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(400)));
+        ;
     }
 
     @Test
     public void shouldThrowBadRequestForInvalidEndDate() throws Exception {
         mockMvc.perform(get("/v1/api/spendings?start-date=01/01/2000"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(400)));
+        ;
     }
 
     @Test
     public void shouldThrowBadRequestForInvalidGranularity() throws Exception {
         mockMvc.perform(get("/v1/api/spendings?granularity=FOO"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(400)));
+        ;
     }
 
     @Test
     public void shouldThrowBadRequestForInvalidGraphType() throws Exception {
-        mockMvc.perform(get("/v1/api/spendings?graph-type=BAZ")).andExpect(status().isBadRequest());
+        mockMvc.perform(get("/v1/api/spendings?graph-type=BAZ"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(400)));
+        ;
     }
 
     @Test
     public void shouldThrowBadRequestForInvalidPage() throws Exception {
-        mockMvc.perform(get("/v1/api/spendings?page=-1")).andExpect(status().isBadRequest());
+        mockMvc.perform(get("/v1/api/spendings?page=-1"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(400)));
+        ;
     }
 
     @Test
     public void shouldThrowBadRequestForLimitTooSmall() throws Exception {
-        mockMvc.perform(get("/v1/api/spendings?limit=0")).andExpect(status().isBadRequest());
+        mockMvc.perform(get("/v1/api/spendings?limit=0"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(400)));
+        ;
     }
 
     @Test
     public void shouldThrowBadRequestForLimitTooLarge() throws Exception {
-        mockMvc.perform(get("/v1/api/spendings?limit=1000")).andExpect(status().isBadRequest());
+        mockMvc.perform(get("/v1/api/spendings?limit=1000"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.httpStatus", Matchers.is(400)));
+    }
+
+    @Test
+    public void shouldMatchRequestedPage() throws Exception {
+        SpendingPageResponse response =
+                new SpendingPageResponse(new PageImpl<>(Collections.emptyList()));
+
+        when(spendingService.getSpendings(any(GetSpendingsRequestFilters.class)))
+                .thenReturn(response);
+
+        mockMvc.perform(get("/v1/api/spendings?page=1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.metadata.currentPage", Matchers.is(1)));
     }
 }
