a1640cb41b03250e65ec515df57be4f40305a1d8
Harjot Singh <hsingh172000@gmail.com>
2025-05-07 22:59:16 -0700

    Start working on a unit test in service layer

--- backend/src/test/java/com/spendingstracker/app/service/SpendingServiceTest.java
+++ backend/src/test/java/com/spendingstracker/app/service/SpendingServiceTest.java
@@ -1,30 +1,113 @@
 package com.spendingstracker.app.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 import com.spendingstracker.app.cache.SpendingCategoryJpaCache;
+import com.spendingstracker.app.constants.Granularity;
+import com.spendingstracker.app.constants.GraphType;
+import com.spendingstracker.app.dto.requests.GetSpendingsRequestFilters;
+import com.spendingstracker.app.dto.response.SpendingPageItemLineChart;
+import com.spendingstracker.app.dto.response.SpendingPageResponse;
 import com.spendingstracker.app.repository.SpendingRepository;
 import com.spendingstracker.app.repository.SpendingUserAggrRepository;
-import com.spendingstracker.app.service.auth.CurrentUserService;
-import com.spendingstracker.app.service.spending.SpendingService;
-import com.spendingstracker.app.service.user.UserService;
+import com.spendingstracker.app.service.auth.CurrentUserServiceImpl;
+import com.spendingstracker.app.service.spending.SpendingServiceImpl;
+import com.spendingstracker.app.service.user.UserServiceImpl;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class SpendingServiceTest {
-    @InjectMocks SpendingService spendingService;
+    @InjectMocks SpendingServiceImpl spendingService;
     @Mock SpendingRepository spendingRepository;
     @Mock SpendingUserAggrRepository spendingUserAggrRepository;
-    @Mock UserService userService;
+    @Mock UserServiceImpl userService;
     @Mock SpendingCategoryJpaCache spendingCategoryJpaCache;
-    @Mock CurrentUserService currentUserService;
+    @Mock CurrentUserServiceImpl currentUserService;
 
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
+        when(currentUserService.getCurrentUserId()).thenReturn(BigInteger.ONE);
+    }
+
+    @Test
+    public void shouldGetLineSpendings() {
+        int N = 10;
+        int pageNum = 0;
+        int pageLimit = 10;
+
+        GetSpendingsRequestFilters filters =
+                new GetSpendingsRequestFilters(
+                        LocalDate.now(),
+                        LocalDate.now(),
+                        Granularity.DAY,
+                        GraphType.LINE,
+                        pageNum,
+                        pageLimit);
+
+        List<SpendingPageItemLineChart> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(
+                    new SpendingPageItemLineChart(
+                            BigInteger.ONE.add(BigInteger.valueOf(i)),
+                            LocalDate.now(),
+                            BigDecimal.TEN.multiply(BigDecimal.valueOf(i))));
+        }
+
+        Page<SpendingPageItemLineChart> page =
+                new PageImpl<>(list, PageRequest.of(pageNum, pageLimit), list.size());
+
+        when(spendingUserAggrRepository.findSpendingsForLineChart(
+                        any(BigInteger.class),
+                        any(LocalDate.class),
+                        any(LocalDate.class),
+                        any(Granularity.class),
+                        any(Pageable.class)))
+                .thenReturn(page);
+
+        SpendingPageResponse response = spendingService.getSpendings(filters);
+
+        assertEquals(response.spendingPage().getTotalElements(), N);
+
+        verify(spendingUserAggrRepository, times(1))
+                .findSpendingsForLineChart(
+                        any(BigInteger.class),
+                        any(LocalDate.class),
+                        any(LocalDate.class),
+                        any(Granularity.class),
+                        any(Pageable.class));
+
+        verify(spendingUserAggrRepository, never())
+                .findSpendingsForBarChart(
+                        any(BigInteger.class),
+                        any(LocalDate.class),
+                        any(LocalDate.class),
+                        any(Granularity.class),
+                        any(Pageable.class));
+
+        verify(spendingUserAggrRepository, never())
+                .findSpendingsForPieChart(
+                        any(BigInteger.class),
+                        any(LocalDate.class),
+                        any(LocalDate.class),
+                        any(Pageable.class));
     }
 }
