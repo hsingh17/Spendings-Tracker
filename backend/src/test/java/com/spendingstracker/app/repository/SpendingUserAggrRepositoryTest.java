8d3f3e9b5cb8e7a74160e420d5ffbeca9182ae34
Harjot Singh <hsingh172000@gmail.com>
2025-05-24 22:23:17 -0700

    Start repo test

--- backend/src/test/java/com/spendingstracker/app/repository/SpendingUserAggrRepositoryTest.java
+++ backend/src/test/java/com/spendingstracker/app/repository/SpendingUserAggrRepositoryTest.java
@@ -1,12 +1,54 @@
 package com.spendingstracker.app.repository;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.spendingstracker.app.constants.SpendingCategoryEnum;
+import com.spendingstracker.app.entity.Spending;
+import com.spendingstracker.app.entity.SpendingCategory;
+import com.spendingstracker.app.entity.SpendingUserAggr;
+import com.spendingstracker.app.entity.User;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SpendingUserAggrRepositoryTest {
+    @Autowired SpendingUserAggrRepository spendingUserAggrRepository;
+
     @Test
-    public void shouldSaveFindDelete() {}
+    public void shouldSaveFindDelete() {
+        // Setup
+        SpendingCategoryEnum[] categoriesEnum = SpendingCategoryEnum.values();
+        List<SpendingCategory> categories = new ArrayList<>();
+        for (SpendingCategoryEnum spendingCategoryEnum : categoriesEnum) {
+            categories.add(new SpendingCategory(spendingCategoryEnum));
+        }
+
+        List<Spending> spendings = new ArrayList<>();
+        for (int i = 0; i < categories.size(); i++) {
+            SpendingCategory category = categories.get(i);
+            spendings.add(
+                    new Spending(category, BigDecimal.TEN.multiply(BigDecimal.valueOf(i)), ""));
+        }
+
+        User user = new User("foo", "foo@bar.com", "bar");
+        SpendingUserAggr spendingUserAggr = new SpendingUserAggr(user, LocalDate.now(), spendings);
+        // Save
+        spendingUserAggr = spendingUserAggrRepository.save(spendingUserAggr);
+        BigInteger id = spendingUserAggr.getSpendingUserAggrId();
+        assertNotNull(id);
+
+        // Delete
+        assertDoesNotThrow(() -> spendingUserAggrRepository.deleteById(id));
+    }
 }
